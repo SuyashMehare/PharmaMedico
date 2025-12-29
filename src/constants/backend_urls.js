6af01f7fd88c1961c85d1c5a74d44ae8f1aaacf7
SuyashMehare <suyashmehare04@gmail.com>
2025-12-30 00:36:04 +0530

    Update backend_urls to fetch from env variables

--- src/constants/backend_urls.js
+++ src/constants/backend_urls.js
@@ -1,46 +1,46 @@
-export const PHARMACY_BACKEND = 'http://localhost:3000/api/v1'
-export const RAZROPAY_BACKEND = 'http://localhost:5222'
+export const PHARMACY_BACKEND = import.meta.env.VITE_PHARMACY_BACKEND || 'http://localhost:3000'
+export const RAZROPAY_BACKEND = import.meta.env.VITE_RAZORPAY_BACKEND || 'http://localhost:5222'
 
 export const ENDPOINTS = {
     auth: {
         admin: {
-            signup: PHARMACY_BACKEND + '/auth/signup',
-            login: PHARMACY_BACKEND + '/auth/login',
-            forgotPassoword: PHARMACY_BACKEND + '/auth/forgot-password',
-            resetPassoword: PHARMACY_BACKEND + '/auth/reset-password/', // :token needed
+            signup: PHARMACY_BACKEND + '/api/v1' + '/auth/signup',
+            login: PHARMACY_BACKEND + '/api/v1' + '/auth/login',
+            forgotPassoword: PHARMACY_BACKEND + '/api/v1' + '/auth/forgot-password',
+            resetPassoword: PHARMACY_BACKEND + '/api/v1' + '/auth/reset-password/', // :token needed
         },
         user: {
-            signup: PHARMACY_BACKEND + '/auth/signup',
-            login: PHARMACY_BACKEND + '/auth/login',
-            forgotPassoword: PHARMACY_BACKEND + '/auth/forgot-password',
-            resetPassoword: PHARMACY_BACKEND + '/auth/reset-password/', // :token needed
+            signup: PHARMACY_BACKEND + '/api/v1' + '/auth/signup',
+            login: PHARMACY_BACKEND + '/api/v1' + '/auth/login',
+            forgotPassoword: PHARMACY_BACKEND + '/api/v1' + '/auth/forgot-password',
+            resetPassoword: PHARMACY_BACKEND + '/api/v1' + '/auth/reset-password/', // :token needed
         }
     },
 
     product: {
         user: {
-            getAllProducts: PHARMACY_BACKEND + '/users',
-            getSingleProdcut: PHARMACY_BACKEND + '/users/single',
-            subscribeProduct: PHARMACY_BACKEND + '/users/product/subscribe',
-            unSubscribeProduct: PHARMACY_BACKEND + '/users/product/unsubscribe'
+            getAllProducts: PHARMACY_BACKEND + '/api/v1' + '/users',
+            getSingleProdcut: PHARMACY_BACKEND + '/api/v1' + '/users/single',
+            subscribeProduct: PHARMACY_BACKEND + '/api/v1' + '/users/product/subscribe',
+            unSubscribeProduct: PHARMACY_BACKEND + '/api/v1' + '/users/product/unsubscribe'
         },
         admin: {
-            getAllProducts: PHARMACY_BACKEND + '/admin',
-            createSingleProduct: PHARMACY_BACKEND + '/admin',
-            createManyProducts: PHARMACY_BACKEND + '/admin/many',
-            updateProduct: PHARMACY_BACKEND + '/admin', // :productId needed
-            deleteProduct: PHARMACY_BACKEND + '/admin', // :productId needed
+            getAllProducts: PHARMACY_BACKEND + '/api/v1' + '/admin',
+            createSingleProduct: PHARMACY_BACKEND + '/api/v1' + '/admin',
+            createManyProducts: PHARMACY_BACKEND + '/api/v1' + '/admin/many',
+            updateProduct: PHARMACY_BACKEND + '/api/v1' + '/admin', // :productId needed
+            deleteProduct: PHARMACY_BACKEND + '/api/v1' + '/admin', // :productId needed
         }
     },
 
     order: {
         admin: {
-            updateOrderStatus: PHARMACY_BACKEND + '/admin/order/status', // :orderId needed
-            fetchExecutableOrders: PHARMACY_BACKEND + '/admin/orders'
+            updateOrderStatus: PHARMACY_BACKEND + '/api/v1' + '/admin/order/status', // :orderId needed
+            fetchExecutableOrders: PHARMACY_BACKEND + '/api/v1' + '/admin/orders'
         },
         user: {
-            createOrder: PHARMACY_BACKEND + '/users/order',
-            getOrderHistory: PHARMACY_BACKEND + '/users/order/history',
+            createOrder: PHARMACY_BACKEND + '/api/v1' + '/users/order',
+            getOrderHistory: PHARMACY_BACKEND + '/api/v1' + '/users/order/history',
             sendOrderAmount: 'http://localhost:5222/rezorpay/sent'
         }
 
