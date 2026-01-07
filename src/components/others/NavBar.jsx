868d9c26a3e131449d4fc4daee354f09af90d70f
SuyashMehare <suyashmehare04@gmail.com>
2026-01-07 16:47:11 +0530

    Fix: login to logout component shift (render/mounting) within navbar resolved

--- src/components/others/NavBar.jsx
+++ src/components/others/NavBar.jsx
@@ -1,32 +1,33 @@
 import { useState, useEffect } from 'react';
-import { 
-  LayoutDashboard, 
-  Users, 
-  Package, 
-  MessageSquare, 
-  ShoppingCart, 
-  Clock, 
-  Bell, 
-  User, 
-  Upload, 
+import {
+  LayoutDashboard,
+  Users,
+  Package,
+  MessageSquare,
+  ShoppingCart,
+  Clock,
+  Bell,
+  User,
+  Upload,
   Activity,
   Menu,
   X,
   LogOut
 } from 'lucide-react';
 import Button from './Button'; // Import your existing Button component
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { FRONTEND_ROUTES } from '../../constants/frontend_urls';
 
 const Navbar = () => {
   const [role, setRole] = useState(null);
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const location  = useLocation();
 
   // Get role from localStorage (simulating your utility function)
   useEffect(() => {
-    const storedRole = localStorage.getItem('role') || 'regular'; // Default to regular if not found
+    const storedRole = localStorage.getItem('pharmacy_role'); // Default to regular if not found
     setRole(storedRole);
-  }, []);
+  },[location.pathname]);
 
   const toggleMobileMenu = () => {
     setMobileMenuOpen(!mobileMenuOpen);
@@ -50,7 +51,13 @@ const Navbar = () => {
 
           {/* Desktop Navigation */}
           <div className="hidden md:flex items-center space-x-4">
-            {role === 'admin' ? (
+            {
+              role == null ?
+                <>
+                  <NavItem href="/products" icon={<Package className="h-5 w-5" />} text="Products" />
+                  <NavItem href={FRONTEND_ROUTES.user_cart} icon={<ShoppingCart className="h-5 w-5" />} text="Cart" />
+                </>
+              : role === 'admin' ? (
               <>
                 <NavItem href="/dashboard" icon={<LayoutDashboard className="h-5 w-5" />} text="Dashboard" />
                 <NavItem href="/orders" icon={<Users className="h-5 w-5" />} text="User Orders" />
@@ -67,32 +74,79 @@ const Navbar = () => {
                 <NavItem href="/upload" icon={<Upload className="h-5 w-5" />} text="Upload Prescription" />
                 <NavItem href="/health" icon={<Activity className="h-5 w-5" />} text="Health Report" />
               </>
-            )}
-            
+            )
+            }
+
             {/* Logout Button - Desktop */}
-            <div className="ml-4">
-              <Button
-                variant="danger"
-                size="sm"
-                icon={<LogOut className="h-4 w-4" />}
-                onClick={handleLogout}
-                className="hidden md:block"
-                iconPosition='mid'
-              />
-            </div>
+            {
+              role ?
+                <div className="ml-4">
+                  <Button
+                    variant="danger"
+                    size="sm"
+                    icon={<LogOut className="h-4 w-4" />}
+                    onClick={handleLogout}
+                    className="hidden md:block"
+                    iconPosition='mid'
+                  />
+                </div> :
+                <div className="flex items-center gap-2 text-sm font-medium">
+                  <Link
+                    href={FRONTEND_ROUTES.login}
+                    className="relative text-white/90 transition-colors duration-200 hover:text-white
+               after:absolute after:left-0 after:-bottom-0.5 after:h-[2px] after:w-0
+               after:bg-white after:transition-all after:duration-300 hover:after:w-full"
+                onClick={() => console.log(FRONTEND_ROUTES.login)}
+                  >
+                    Login
+                  </Link>
+
+                  <span className="text-white/60">/</span>
+
+                  <Link
+                    href={FRONTEND_ROUTES.login}
+                    className="relative text-white/90 transition-colors duration-200 hover:text-white
+               after:absolute after:left-0 after:-bottom-0.5 after:h-[2px] after:w-0
+               after:bg-white after:transition-all after:duration-300 hover:after:w-full"
+                  >
+                    Sign Up
+                  </Link>
+                </div>
+
+            }
           </div>
 
           {/* Mobile menu button */}
           <div className="md:hidden flex items-center space-x-2">
             {/* Logout Button - Mobile (hidden on desktop) */}
-            <Button
+            {role ? <Button
               variant="danger"
               size="sm"
               icon={<LogOut className="h-4 w-4" />}
               onClick={handleLogout}
               className="md:hidden"
-            />
-            
+            />: <div className="flex items-center gap-2 text-sm font-medium">
+                  <Link
+                    href={FRONTEND_ROUTES.login}
+                    className="relative text-white/90 transition-colors duration-200 hover:text-white
+               after:absolute after:left-0 after:-bottom-0.5 after:h-[2px] after:w-0
+               after:bg-white after:transition-all after:duration-300 hover:after:w-full"
+                  >
+                    Login
+                  </Link>
+
+                  <span className="text-white/60">/</span>
+
+                  <Link
+                    href={FRONTEND_ROUTES.login}
+                    className="relative text-white/90 transition-colors duration-200 hover:text-white
+               after:absolute after:left-0 after:-bottom-0.5 after:h-[2px] after:w-0
+               after:bg-white after:transition-all after:duration-300 hover:after:w-full"
+                  >
+                    Sign Up
+                  </Link>
+                </div>}
+
             <button
               onClick={toggleMobileMenu}
               className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none"
@@ -111,7 +165,13 @@ const Navbar = () => {
       {mobileMenuOpen && (
         <div className="md:hidden bg-gray-800">
           <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-            {role === 'admin' ? (
+            {
+              role == null ?
+              <>
+                <MobileNavItem href="/products" icon={<Package className="h-5 w-5" />} text="Products" />
+                <MobileNavItem href={FRONTEND_ROUTES.user_cart} icon={<ShoppingCart className="h-5 w-5" />} text="Cart" />
+              </> :
+              role === 'admin' ? (
               <>
                 <MobileNavItem href="/dashboard" icon={<LayoutDashboard className="h-5 w-5" />} text="Dashboard" />
                 <MobileNavItem href="/orders" icon={<Users className="h-5 w-5" />} text="User Orders" />
@@ -128,18 +188,42 @@ const Navbar = () => {
                 <MobileNavItem href="/upload" icon={<Upload className="h-5 w-5" />} text="Upload Prescription" />
                 <MobileNavItem href="/health" icon={<Activity className="h-5 w-5" />} text="Health Report" />
               </>
-            )}
-            
+            )
+            }
+
             {/* Mobile Logout Button */}
-            <div className="px-3 py-2">
-              <Button
+            { role ?
+              <div className="px-3 py-2">
+              {role && <Button
                 variant="danger"
                 size="sm"
                 icon={<LogOut className="h-4 w-4" />}
                 onClick={handleLogout}
                 fullWidth
-              />
-            </div>
+              />}
+            </div> : 
+              <div className="flex items-center gap-2 text-sm font-medium">
+                  <Link
+                    href={FRONTEND_ROUTES.login}
+                    className="relative text-white/90 transition-colors duration-200 hover:text-white
+               after:absolute after:left-0 after:-bottom-0.5 after:h-[2px] after:w-0
+               after:bg-white after:transition-all after:duration-300 hover:after:w-full"
+                  >
+                    Login
+                  </Link>
+
+                  <span className="text-white/60">/</span>
+
+                  <Link
+                    href={FRONTEND_ROUTES.login}
+                    className="relative text-white/90 transition-colors duration-200 hover:text-white
+               after:absolute after:left-0 after:-bottom-0.5 after:h-[2px] after:w-0
+               after:bg-white after:transition-all after:duration-300 hover:after:w-full"
+                  >
+                    Sign Up
+                  </Link>
+                </div>
+            }
           </div>
         </div>
       )}
@@ -148,7 +232,7 @@ const Navbar = () => {
 };
 
 // Reusable Nav Item Component for Desktop
-const NavItem = ({ href, icon, text }) => {
+const NavItem = ({ href, icon = null, text }) => {
   return (
     <Link
       to={href}
